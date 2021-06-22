3339bd97b9cb537364f70784f458691da480b54c
PhilipLawson <phil@uberdesigns.co.uk>
2021-06-22 16:55:56 +0100

    Added a very basic character controller scene

---
+++ UnityExamples/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    bool canJump=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(5.0f,0.0f,0.0f);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(-5.0f,0.0f,0.0f);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0.0f,0.0f,5.0f);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0.0f,0.0f,-5.0f);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            canJump = false; //change to false so no air jumping
+            //Add velocity to the rigidbody
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0,5.0f,0);
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        // This checks to see if the object has touched the ground and if so,
+        // sets the canJump bool back to true to allow jumping.
+        if(other.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Respawn")
+        {
+            // If the cube touches the respawn trigger, we turn
+            // make it a kinematic RB, move it, then re-enable
+            // rigidbody.
+            this.GetComponent<Rigidbody>().isKinematic = true;
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+            this.GetComponent<Rigidbody>().position = new Vector3(0,0.5f,0f);
+            this.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
