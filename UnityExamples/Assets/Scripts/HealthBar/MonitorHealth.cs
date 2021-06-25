c920867ca498ff7b945d0254e041b6b3a9d41ea6
PhilipLawson <phil@uberdesigns.co.uk>
2021-06-25 21:54:20 +0100

    Updated the comments

--- UnityExamples/Assets/Scripts/HealthBar/MonitorHealth.cs
+++ UnityExamples/Assets/Scripts/HealthBar/MonitorHealth.cs
@@ -17,27 +17,31 @@ public class MonitorHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If the players health is between 65-99
         if(playerHealth < 100 && playerHealth >=65)
         {
-            //Destroy(heartIcons[heartIcons.Length-1]);
+            //Disable the last heart icon in the array.
             heartIcons[heartIcons.Length-1].enabled = false;
         }
         if(playerHealth <=64 && playerHealth >=30)
         {
-            //Destroy(heartIcons[heartIcons.Length-2]);
+            //Disable the second heart icon in the array.
             heartIcons[heartIcons.Length-2].enabled = false;
         }
         if(playerHealth <=29)
         {
-            //Destroy(heartIcons[heartIcons.Length-3]);
+            //Disable the first heart icon in the array.
             heartIcons[heartIcons.Length-3].enabled = false;
+            // Disable the player move script
             Player.GetComponent<MovePlayer>().enabled = false;
+            // After 1 second, run the kill player function.
             Invoke("killPlayer", 1.0f);
         }
     }
 
     void killPlayer()
     {
+        // This basically sets the scene back to defaults and then calls the respawn function in the MovePlayer script.
         playerHealth = 100;
         Player.GetComponent<MovePlayer>().enabled = true;
         heartIcons[heartIcons.Length-1].enabled = true;
