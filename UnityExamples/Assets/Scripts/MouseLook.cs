93305fe1abb9e0a9a783b138cc5fd4206bc7d55e
PhilipLawson <phil@uberdesigns.co.uk>
2021-06-26 12:55:42 +0100

    Created a basic character setup

--- UnityExamples/Assets/Scripts/MouseLook.cs
+++ UnityExamples/Assets/Scripts/MouseLook.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour {
 	Vector2 rotation = Vector2.zero;
-	public float speed = 3;
+	public float speed = 1;
+    public GameObject player;
 
 	void Update () 
     {
@@ -19,5 +20,8 @@ public class MouseLook : MonoBehaviour {
         rotation.y += Input.GetAxis ("Mouse X");
 		rotation.x += -Input.GetAxis ("Mouse Y");
 		transform.eulerAngles = (Vector2)rotation * speed;
+        //player.transform.eulerAngles = (Vector2)rotation * speed;
+        Vector3 temp = new Vector3(rotation.x * speed, 0, rotation.y * speed);
+        player.GetComponent<Rigidbody>().rotation = Quaternion.LookRotation(temp, Vector3.up);
     }
 }
