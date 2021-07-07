de9d23ab16cb5cdd91fef741d9f37bddfcddbed4
PhilipLawson <phil@uberdesigns.co.uk>
2021-07-07 14:47:02 +0100

    Added a blood spray particle scene example

---
+++ UnityExamples/Assets/Scripts/ParticleControls/PlayParticles.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticles : MonoBehaviour
+{
+    public ParticleSystem blood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!blood.isPlaying)
+            {
+                blood.Play();
+            }
+        }
+    }
+}
