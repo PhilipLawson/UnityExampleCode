41b4f7e1a67bcfd6f7c9016b6e1346208a980b46
PhilipLawson <phil@uberdesigns.co.uk>
2021-12-15 13:51:08 +0000

    Added a breaking sound effect

--- UnityExamples/Assets/Scripts/SquidGame/DestroyGlass.cs
+++ UnityExamples/Assets/Scripts/SquidGame/DestroyGlass.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class DestroyGlass : MonoBehaviour
 {
     public GameObject Player;
+    public AudioSource glassbreak;
+
     void OnCollisionEnter(Collision other)
     {
         if ((other.gameObject.tag == "Player") && (this.gameObject.tag=="Enemy"))
         {
             //Player.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             //Player.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            if(!glassbreak.isPlaying)
+            {
+                glassbreak.Play();
+            }
             Destroy(this.gameObject);
         }
     }
